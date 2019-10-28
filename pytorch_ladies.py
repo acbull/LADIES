020a4355767a2f6925fe5db80d19ba2ee896d124
Ziniu Hu <31607751+acbull@users.noreply.github.com>
2019-10-28 01:24:08 -0700

    Add files via upload

---
+++ pytorch_ladies.py
@@ -0,0 +1,266 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from utils import *
+from tqdm import tqdm
+import argparse
+
+
+# In[2]:
+
+
+import sys; sys.argv=['']; del sys
+import warnings
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+
+
+# In[3]:
+
+
+parser = argparse.ArgumentParser(description='Training GCN on Cora/CiteSeer/PubMed Datasets')
+
+'''
+    Dataset arguments
+'''
+parser.add_argument('--dataset', type=str, default='reddit',
+                    help='Dataset name: Cora/CiteSeer/PubMed')
+parser.add_argument('--nhid', type=int, default=128,
+                    help='Hidden state dimension')
+parser.add_argument('--epoch_num', type=int, default= 100,
+                    help='Number of Epoch')
+parser.add_argument('--pool_num', type=int, default= 4,
+                    help='Number of Pool')
+parser.add_argument('--batch_num', type=int, default= 32,
+                    help='Maximum Batch Number')
+parser.add_argument('--batch_size', type=int, default=512,
+                    help='size of output node in a batch')
+parser.add_argument('--n_layers', type=int, default=5,
+                    help='Number of GCN layers')
+parser.add_argument('--samp_num', type=int, default=64,
+                    help='Number of sampled nodes per layer')
+parser.add_argument('--sample_method', type=str, default='ladies',
+                    help='Sampled Algorithms: ladies/fastgcn/full')
+parser.add_argument('--cuda', type=int, default=-1,
+                    help='Avaiable GPU ID')
+
+
+# In[4]:
+
+
+args = parser.parse_args()
+
+
+# In[5]:
+
+
+class GraphConvolution(nn.Module):
+    def __init__(self, n_in, n_out, bias=True):
+        super(GraphConvolution, self).__init__()
+        self.n_in  = n_in
+        self.n_out = n_out
+        self.linear = nn.Linear(n_in,  n_out)
+    def forward(self, x, adj):
+        out = self.linear(x)
+        return F.relu(torch.spmm(adj, out))
+
+class GCN(nn.Module):
+    def __init__(self, nfeat, nhid, layers, dropout):
+        super(GCN, self).__init__()
+        self.layers = layers
+        self.nhid = nhid
+        self.gcs = nn.ModuleList()
+        self.gcs.append(GraphConvolution(nfeat,  nhid))
+        self.dropout = nn.Dropout(dropout)
+        for i in range(layers-1):
+            self.gcs.append(GraphConvolution(nhid,  nhid))
+    def forward(self, x, adjs):
+        for idx in range(len(self.gcs)):
+            x = self.dropout(self.gcs[idx](x, adjs[idx]))
+        return x
+    def hier_forward(self, x, adjs):
+        res = [x]
+        for idx in range(len(self.gcs)):
+            x = self.gcs[idx](x, adjs[idx])
+            res += [x.detach()]
+        return res  
+    def cal_mamory(self, x, adjs):
+        res = self.hier_forward(x, adjs)
+        sz = 0
+        for ri in res:
+            sz += np.prod(np.array(ri.shape))
+        for ai in adjs:
+            sz += (ai.to_dense() > 0).sum().tolist()
+        for pi in self.parameters():
+            sz += np.prod(np.array(pi.shape))
+        return sz * 32 / 1024 / 1024 / 8
+    
+class SuGCN(nn.Module):
+    def __init__(self, encoder, num_classes, dropout, inp):
+        super(SuGCN, self).__init__()
+        self.encoder = encoder
+        self.dropout = nn.Dropout(dropout)
+        self.linear  = nn.Linear(self.encoder.nhid, num_classes)
+    def forward(self, feat, adjs):
+        x = self.dropout(self.encoder(feat, adjs))
+        x = self.linear(x)
+        return F.log_softmax(x, dim=1)
+
+
+# In[6]:
+
+
+def fastgcn_sampler(seed, batch_nodes, samp_num_list, num_nodes, lap_matrix, depth):
+    np.random.seed(seed)
+    previous_nodes = batch_nodes
+    adjs  = []
+    pi = np.array(np.sum(lap_matrix.multiply(lap_matrix), axis=0))[0]
+    p = pi / np.sum(pi)
+    for d in range(depth):
+        U = lap_matrix[previous_nodes , :]
+        s_num = np.min([np.sum(p > 0), samp_num_list[d]])
+        after_nodes = np.random.choice(num_nodes, s_num, p = p, replace = False)
+        adj = row_norm(U[: , after_nodes].multiply(1/p[after_nodes]))
+        sparse_mx = adj.tocoo().astype(np.float32)
+        adjs += [sparse_mx_to_torch_sparse_tensor(row_norm(adj))]
+        previous_nodes = after_nodes
+    adjs.reverse()
+    return adjs, previous_nodes, batch_nodes
+
+def ladies_sampler(seed, batch_nodes, samp_num_list, num_nodes, lap_matrix, depth):
+    np.random.seed(seed)
+    previous_nodes = batch_nodes
+    adjs  = []
+    for d in range(depth):
+        U = lap_matrix[previous_nodes , :]
+        pi = np.array(np.sum(U.multiply(U), axis=0))[0]
+        p = pi / np.sum(pi)
+        s_num = np.min([np.sum(p > 0), samp_num_list[d]])
+        after_nodes = np.random.choice(num_nodes, s_num, p = p, replace = False)
+        adj = U[: , after_nodes].multiply(1/p[after_nodes])
+        adjs += [sparse_mx_to_torch_sparse_tensor(row_norm(adj))]
+        previous_nodes = after_nodes
+    adjs.reverse()
+    return adjs, previous_nodes, batch_nodes
+def default_sampler(seed, batch_nodes, samp_num_list, num_nodes, lap_matrix, depth):
+    mx = sparse_mx_to_torch_sparse_tensor(lap_matrix)
+    return [mx for i in range(depth)], np.arange(num_nodes), batch_nodes
+def prepare_data(pool, process_ids, train_nodes, samp_num_list, num_nodes, lap_matrix, depth):
+    jobs = []
+    for _ in process_ids:
+        idx = torch.randperm(len(train_nodes))[:args.batch_size]
+        batch_nodes = train_nodes[idx]
+        p = pool.apply_async(ladies_sampler, args=(np.random.randint(2**32 - 1), batch_nodes,                                                    samp_num_list, num_nodes, lap_matrix, depth))
+        jobs.append(p)
+    return jobs
+def package_mxl(mxl, device):
+    return [torch.sparse.FloatTensor(mx[0], mx[1], mx[2]).to(device) for mx in mxl]
+
+
+# In[7]:
+
+
+if args.cuda != -1:
+    device = torch.device("cuda:" + args.cuda)
+else:
+    device = torch.device("cpu")
+# edges, labels, feat_data, num_classes, train_nodes, valid_nodes, test_nodes = load_data(args.dataset)
+edges, labels, feat_data, num_classes, train_nodes, valid_nodes, test_nodes = load_data('cora')
+
+
+# In[8]:
+
+
+adj_matrix = get_adj(edges, feat_data.shape[0])
+adj_matrix = adj_matrix + sp.eye(adj_matrix.shape[0])
+lap_matrix = normalize(adj_matrix)
+
+if type(feat_data) == scipy.sparse.lil.lil_matrix:
+    feat_data = torch.FloatTensor(feat_data.todense()).to(device) 
+else:
+    feat_data = torch.FloatTensor(feat_data).to(device)
+labels    = torch.LongTensor(labels).to(device) 
+
+
+# In[9]:
+
+
+if args.sample_method == 'ladies':
+    sampler = ladies_sampler
+elif args.sample_method == 'fastgcn':
+    sampler = fastgcn_sampler
+elif args.sample_method == 'full':
+    sampler = default_sample
+
+
+# In[ ]:
+
+
+encoder = GCN(nfeat = feat_data.shape[1], nhid=args.nhid, layers=args.n_layers, dropout = 0.3).to(device)
+susage  = SuGCN(encoder = encoder, num_classes=num_classes, dropout=0.3, inp = feat_data.shape[1])
+susage.to(device)
+
+optimizer = optim.Adam(filter(lambda p : p.requires_grad, susage.parameters()))
+samp_num_list = [64 for i in range(args.n_layers)]
+
+std_adjs, _, _ = default_sampler(_, _, _, 0, lap_matrix, args.n_layers)
+std_adjs = package_mxl(std_adjs, device)
+times = []
+res = []
+process_ids = np.arange(args.batch_num // 8)
+for _ in range(5):
+    pool = mp.Pool(args.pool_num)
+    jobs = prepare_data(pool, process_ids, train_nodes, samp_num_list, len(feat_data), lap_matrix, args.n_layers)
+    for epoch in tqdm(np.arange(args.epoch_num)+1, desc='Training'):
+        train_data = [job.get() for job in jobs]
+        pool.close()
+        pool.join()
+        pool = mp.Pool(args.pool_num)
+        jobs = prepare_data(pool, process_ids, train_nodes, samp_num_list, len(feat_data), lap_matrix, args.n_layers)
+        for _iter in np.arange(2):
+            for adjs, input_nodes, output_nodes in train_data:    
+                adjs = package_mxl(adjs, device)
+                optimizer.zero_grad()
+                t1 = time.time()
+                susage.train()
+                output = susage.forward(feat_data[input_nodes], adjs)
+                loss_train = F.cross_entropy(output, labels[output_nodes])
+                loss_train.backward()
+                optimizer.step()
+                times += [time.time() - t1]
+                del loss_train
+            torch.cuda.empty_cache()
+        if epoch % 5 == 0:
+            susage.eval()
+            pred = susage.forward(feat_data, std_adjs)
+            trai_label = labels[train_nodes]
+            trai_pred = pred[train_nodes].cpu().detach().numpy()
+            trai_f1 = f1_score(np.argmax(trai_pred, axis=1), trai_label.cpu().detach().numpy(), average='micro')    
+
+            vald_label = labels[valid_nodes]
+            vald_pred = pred[valid_nodes].cpu().detach().numpy()
+            vald_f1 = f1_score(np.argmax(vald_pred, axis=1), vald_label.cpu().detach().numpy(), average='micro')
+
+            test_label = labels[test_nodes]
+            test_pred = pred[test_nodes].cpu().detach().numpy()
+            test_f1 = f1_score(np.argmax(test_pred, axis=1), test_label.cpu().detach().numpy(), average='micro')
+            res += [[trai_f1, epoch, 'train']]
+            res += [[vald_f1, epoch, 'valid']]
+            res += [[test_f1, epoch, 'test']]
+
+
+# In[ ]:
+
+
+dt = pd.DataFrame(res, columns=['f1-score', 'batch', 'type'])
+
+
+# In[ ]:
+
+
+sb.lineplot(data = dt, x='batch', y='f1-score', hue='type')
+plt.legend(loc='lower right')
+plt.show()
+
---
+++ utils.py
@@ -0,0 +1,266 @@
+import numpy as np
+import scipy.sparse as sp
+import torch
+import sys
+import pickle as pkl
+import scipy.io as sio
+import networkx as nx
+from collections import defaultdict
+import torch.nn as nn
+import torch
+from torch.nn import init
+from torch.autograd import Variable
+import torch.nn.functional as F
+import torch.optim as optim
+from copy import deepcopy
+from sklearn.preprocessing import StandardScaler
+from networkx.readwrite import json_graph
+import json
+import pandas as pd
+from sklearn.metrics import f1_score
+from collections import defaultdict
+from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
+import networkx as nx
+import time
+import sys
+import os
+
+
+
+def parse_index_file(filename):
+    """Parse index file."""
+    index = []
+    for line in open(filename):
+        index.append(int(line.strip()))
+    return index
+
+def load_data(dataset_str):
+
+    
+    if dataset_str == 'ppi':
+        prefix = './ppi/ppi'
+        G_data = json.load(open(prefix + "-G.json"))
+        G = json_graph.node_link_graph(G_data)
+        if isinstance(G.nodes()[0], int):
+            conversion = lambda n : int(n)
+        else:
+            conversion = lambda n : n
+
+        if os.path.exists(prefix + "-feats.npy"):
+            feats = np.load(prefix + "-feats.npy")
+        else:
+            print("No features present.. Only identity features will be used.")
+            feats = None
+        id_map = json.load(open(prefix + "-id_map.json"))
+        id_map = {conversion(k):int(v) for k,v in id_map.items()}
+        walks = []
+        class_map = json.load(open(prefix + "-class_map.json"))
+        if isinstance(list(class_map.values())[0], list):
+            lab_conversion = lambda n : n
+        else:
+            lab_conversion = lambda n : int(n)
+
+        class_map = {conversion(k):lab_conversion(v) for k,v in class_map.items()}
+
+        ## Remove all nodes that do not have val/test annotations
+        ## (necessary because of networkx weirdness with the Reddit data)
+        broken_count = 0
+        for node in G.nodes():
+            if not 'val' in G.node[node] or not 'test' in G.node[node]:
+                G.remove_node(node)
+                broken_count += 1
+        print("Removed {:d} nodes that lacked proper annotations due to networkx versioning issues".format(broken_count))
+
+        ## Make sure the graph has edge train_removed annotations
+        ## (some datasets might already have this..)
+        print("Loaded data.. now preprocessing..")
+        for edge in G.edges():
+            if (G.node[edge[0]]['val'] or G.node[edge[1]]['val'] or
+                G.node[edge[0]]['test'] or G.node[edge[1]]['test']):
+                G[edge[0]][edge[1]]['train_removed'] = True
+            else:
+                G[edge[0]][edge[1]]['train_removed'] = False
+
+        train_ids = np.array([id_map[str(n)] for n in G.nodes() if not G.node[n]['val'] and not G.node[n]['test']])
+        train_feats = feats[train_ids]
+        scaler = StandardScaler()
+        scaler.fit(train_feats)
+        features = scaler.transform(feats)
+        
+        degrees = np.zeros(len(G), dtype=np.int64)
+        edges = []
+        labels = []
+        idx_train = []
+        idx_val   = []
+        idx_test  = []
+        for s in G:
+            if G.nodes[s]['test']:
+                idx_test += [s]
+            elif G.nodes[s]['val']:
+                idx_val += [s]
+            else:
+                idx_train += [s]
+            for t in G[s]:
+                edges += [[s, t]]
+            degrees[s] = len(G[s])
+            labels += [class_map[str(s)]]
+        
+        return np.array(edges), np.array(degrees), np.array(labels), np.array(features),\
+                np.array(idx_train), np.array(idx_val), np.array(idx_test)
+    
+    names = ['x', 'y', 'tx', 'ty', 'allx', 'ally', 'graph']
+    objects = []
+    for i in range(len(names)):
+        with open("data/ind.{}.{}".format(dataset_str, names[i]), 'rb') as f:
+            if sys.version_info > (3, 0):
+                objects.append(pkl.load(f, encoding='latin1'))
+            else:
+                objects.append(pkl.load(f))
+
+    x, y, tx, ty, allx, ally, graph = tuple(objects)
+    test_idx_reorder = parse_index_file("data/ind.{}.test.index".format(dataset_str))
+    test_idx_range = np.sort(test_idx_reorder)
+
+    if dataset_str == 'citeseer':
+        # Fix citeseer dataset (there are some isolated nodes in the graph)
+        # Find isolated nodes, add them as zero-vecs into the right position
+        test_idx_range_full = range(min(test_idx_reorder), max(test_idx_reorder)+1)
+        tx_extended = sp.lil_matrix((len(test_idx_range_full), x.shape[1]))
+        tx_extended[test_idx_range-min(test_idx_range), :] = tx
+        tx = tx_extended
+        ty_extended = np.zeros((len(test_idx_range_full), y.shape[1]))
+        ty_extended[test_idx_range-min(test_idx_range), :] = ty
+        ty = ty_extended
+
+    features = sp.vstack((allx, tx)).tolil()
+    features[test_idx_reorder, :] = features[test_idx_range, :]
+
+    labels = np.vstack((ally, ty))
+    labels[test_idx_reorder, :] = labels[test_idx_range, :]
+
+    idx_test = np.array(test_idx_range.tolist())
+    idx_train = np.array(range(len(y)))
+    idx_val = np.array(range(len(y), len(y)+500))
+
+
+    degrees = np.zeros(len(labels), dtype=np.int64)
+    edges = []
+    for s in graph:
+        for t in graph[s]:
+            edges += [[s, t]]
+        degrees[s] = len(graph[s])
+
+    return np.array(edges), degrees, labels, features,  idx_train, idx_val, idx_test
+
+def load_cora():
+    num_nodes = 2708
+    num_feats = 1433
+    feat_data = np.zeros((num_nodes, num_feats))
+    labels = np.empty((num_nodes,1), dtype=np.int64)
+    node_map = {}
+    label_map = {}
+    with open("cora/cora.content") as fp:
+        for i,line in enumerate(fp):
+            info = line.strip().split()
+            feat_data[i,:] = info[1:-1]
+            node_map[info[0]] = i
+            if not info[-1] in label_map:
+                label_map[info[-1]] = len(label_map)
+            labels[i] = label_map[info[-1]]
+    degrees = np.zeros(num_nodes, dtype=np.int64)
+    adj_lists = []
+    with open("cora/cora.cites") as fp:
+        for i,line in enumerate(fp):
+            info = line.strip().split()
+            paper1 = node_map[info[0]]
+            paper2 = node_map[info[1]]
+            adj_lists += [[paper1, paper2]]
+            adj_lists += [[paper2, paper1]]
+            degrees[paper1] += 1
+            degrees[paper2] += 1
+    adj_lists = np.array(adj_lists)
+    return feat_data, labels, adj_lists, np.array(degrees)
+
+def normalize(mx):
+    """Sym-normalize sparse matrix"""
+    rowsum = np.array(mx.sum(1))
+    r_inv = np.power(rowsum, -1/2).flatten()
+    r_inv[np.isinf(r_inv)] = 0.
+    r_mat_inv = sp.diags(r_inv)
+    
+    colsum = np.array(mx.sum(0))
+    c_inv = np.power(colsum, -1/2).flatten()
+    c_inv[np.isinf(c_inv)] = 0.
+    c_mat_inv = sp.diags(c_inv)
+    
+    mx = r_mat_inv.dot(mx).dot(c_mat_inv)
+    return mx
+
+def row_norm(mx):
+    rowsum = np.array(mx.sum(1))
+    r_inv = np.power(rowsum, -1).flatten()
+    r_inv[np.isinf(r_inv)] = 0.
+    r_mat_inv = sp.diags(r_inv)
+
+    mx = r_mat_inv.dot(mx)
+    return mx
+
+
+def generate_random_graph(n, e, prob = 0.1):
+    idx = np.random.randint(2)
+    g = nx.powerlaw_cluster_graph(n, e, prob) 
+    adj_lists = defaultdict(set)
+    num_feats = 8
+    degrees = np.zeros(len(g), dtype=np.int64)
+    edges = []
+    for s in g:
+        for t in g[s]:
+            edges += [[s, t]]
+            degrees[s] += 1
+            degrees[t] += 1
+    edges = np.array(edges)
+    return degrees, edges, g, None 
+
+def get_sparse(edges, num_nodes):
+    adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
+                    shape=(num_nodes, num_nodes), dtype=np.float32)
+    adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+    adj = normalize(adj + sp.eye(adj.shape[0]))
+    return sparse_mx_to_torch_sparse_tensor(adj) 
+
+def normalize(mx):
+    """Row-normalize sparse matrix"""
+    rowsum = np.array(mx.sum(1))
+    r_inv = np.power(rowsum, -1).flatten()
+    r_inv[np.isinf(r_inv)] = 0.
+    r_mat_inv = sp.diags(r_inv)
+    mx = r_mat_inv.dot(mx)
+    return mx
+
+def norm(l):
+    return (l - np.average(l)) / np.std(l)
+
+def stat(l):
+    return np.average(l), np.sqrt(np.var(l))
+
+def sparse_mx_to_torch_sparse_tensor(sparse_mx):
+    """Convert a scipy sparse matrix to a torch sparse tensor."""
+    sparse_mx = sparse_mx.tocoo().astype(np.float32)
+    if len(sparse_mx.row) == 0 and len(sparse_mx.col) == 0:
+        indices = torch.LongTensor([[], []])
+    else:
+        indices = torch.from_numpy(
+            np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
+    values = torch.from_numpy(sparse_mx.data)
+    shape = torch.Size(sparse_mx.shape)
+    return indices, values, shape
+
+
+def get_adj(edges, num_nodes):
+    adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
+                    shape=(num_nodes, num_nodes), dtype=np.float32)
+    return adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+def get_laplacian(adj):
+    adj = normalize(adj + sp.eye(adj.shape[0]))
+    return sparse_mx_to_torch_sparse_tensor(adj) 
