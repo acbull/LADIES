fixed sampling probability calculation

pytorch_ladies.py
@@ -136,12 +136,12 @@ def ladies_sampler(seed, batch_nodes, samp_num_list, num_nodes, lap_matrix, dept
         Sample nodes from top to bottom, based on the probability computed adaptively (layer-dependent).
     '''
     for d in range(depth):
-        #     row-select the lap_matrix (U) by previously sampled nodes
-        U = lap_matrix[previous_nodes , :]
-        #     Only use the upper layer's neighborhood to calculate the probability.
-        pi = np.array(np.sum(U, axis=0))[0]
-        p = pi / np.sum(pi)
-        s_num = np.min([np.sum(p > 0), samp_num_list[d]])
+        # Q(l)P Matrix ^2
+        u = np.square(lap_matrix[previous_nodes, :])
+        # Take the L2 norm
+        pi = u/np.sum(u, axis= 0)
+        s_num = np.min([np.sum(pi > 0), samp_num_list[d]])
+
         #     sample the next layer's nodes based on the adaptively probability (p).
         after_nodes = np.random.choice(num_nodes, s_num, p = p, replace = False)
         #     Add output nodes for self-loop